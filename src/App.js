80f28374f162745ebed955b480e857c4d3da0431
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-17 21:19:52 +0200

    Try post API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [games, setGames] = useState(null);
+  const [newGame, setNewGame] = useState('');
+  const [id, setId] = useState(3);
 
   const getGames = useCallback (
       async (url, method = 'GET') => {
@@ -30,9 +32,39 @@ function App() {
         fetchGames();
     }, [getGames]);
 
+  const addGame = async () => {
+      try {
+          let response = await fetch(
+              'http://localhost:5000/api/games', {
+                  method: 'POST',
+                  headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                      id: id,
+                      title: newGame
+                  })
+              }
+          );
+
+          let responseData = await response.json();
+          setGames(responseData.games);
+      } catch (err) {}
+
+      setId(id + 1);
+  }
+
+  const handleChange = event => {
+      let writenGame = event.target.value;
+      setNewGame(writenGame);
+  }
+
     return (
         <div className="App">
             <p>Welcome !</p>
+            <input type="text" value={newGame} onChange={handleChange} />
+            <button onClick={addGame}>Add game</button>
             <ul>
                 {games && games.map((game) => {
                     return <li id={game.id}>{game.title}</li>
