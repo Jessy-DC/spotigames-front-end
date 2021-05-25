9ed8895402a835435fffd55929df539867fd95b3
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-25 17:58:01 +0200

    Beginning to dev auth functionnality on front

--- src/App.js
+++ src/App.js
@@ -1,72 +1,32 @@
-import React, {useEffect, useState, useCallback} from 'react';
-import {useHttpClient} from "./hooks/http-hook";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
 import './css/spotigames.css';
 import Navbar from "./components/navbar";
+import { AuthContext } from "./context/auth-context";
+import { useAuth } from "./hooks/auth-hook";
 
 function App() {
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const [games, setGames] = useState(null);
-  const [newGame, setNewGame] = useState('');
-  const [id, setId] = useState(3);
-
-    useEffect(() => {
-        const fetchGames = async () => {
-            try {
-                const responseData = await sendRequest(
-                    'http://localhost:5000/api/games'
-                );
-
-                setGames(responseData.games);
-            } catch (err) {}
-        };
-        fetchGames();
-    }, [sendRequest]);
-
-  const addGame = async event => {
-      event.preventDefault();
-      try {
-          let response = await sendRequest(
-              'http://localhost:5000/api/games',
-              'POST',
-              JSON.stringify({
-                  id: id,
-                  title: newGame
-              }),
-              {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-          });
-
-          setGames(response.games);
-      } catch (err) {}
-
-      setId(id + 1);
-  }
-
-  const handleChange = event => {
-      let writenGame = event.target.value;
-      setNewGame(writenGame);
-  }
+    const { token, login, logout, userId } = useAuth();
 
     return (
-        <div className="App">
-            <Navbar />
-            <p>Welcome !</p>
-            <form method='POST' onSubmit={addGame}>
-                <input type="text" value={newGame} onChange={handleChange} />
-                <button type="submit">Add game</button>
-            </form>
+        <AuthContext.Provider value={{
+            isLoggedIn: !!token,
+            token: token,
+            userId: userId,
+            login: login,
+            logout: logout }}
+        >
+            <Router >
+                <div>
+                    <Navbar />
+                    <p>Welcome !</p>
+                </div>
+            </Router>
 
-            <ul>
-                {games && games.map((game) => {
-                    return <li id={game.id}>{game.title}</li>
-                })}
-            </ul>
-        </div>
+        </AuthContext.Provider>
     )
-
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+
+import '../css/navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
                 <a className="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" />
+                    <img src="https://bulma.io/images/bulma-logo-white.png" width="112" height="28" alt="logo" />
                 </a>
+            </div>
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <NavLink to="/auth">Authenticate</NavLink>
+                </div>
             </div>
         </nav>
     )
