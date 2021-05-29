cdc54bb31d11c28d36d45e0bdb32138ef8b14c44
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-29 23:05:49 +0200

    Change colors

--- src/user/Auth.js
+++ src/user/Auth.js
@@ -8,12 +8,12 @@ const Auth = () => {
             <h2>Please login</h2>
             <div className="field">
                 <div className="control">
-                    <input className="input" type="text" placeholder="Name" />
+                    <input className="input is-primary" type="text" placeholder="Name" />
                 </div>
             </div>
             <div className="field">
                 <p className="control has-icons-left has-icons-right">
-                    <input className="input" type="email" placeholder="Email" />
+                    <input className="input is-primary" type="email" placeholder="Email" />
                     <span className="icon is-left">
                       <ion-icon name="mail-outline">Email</ion-icon>
                     </span>
