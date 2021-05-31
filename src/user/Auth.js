e1d057748afaaca228b4351b0cb523c584ae8c55
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-31 20:47:09 +0200

    Continue form for auth

--- src/user/Auth.js
+++ src/user/Auth.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 
 import './Auth.css'
+import {AuthContext} from "../context/auth-context";
+import {useHttpClient} from "../hooks/http-hook";
+import {useForm} from "../hooks/form-hook";
 
 const Auth = () => {
+    const auth = useContext(AuthContext);
+    const {isLoginMode, setIsLoginMode} = useState(true);
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
+
+    const [formState, inputHandler, setFormData] = useForm({
+        email: {
+            value: '',
+            isValid: false
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    });
+
+    const authSubmitHandler = async event => {
+        event.preventDefault();
+
+        if (isLoginMode) {
+            try {
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/login',
+                    'POST',
+                    JSON.stringify({
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    }),
+                    {
+                        'Content-Type': 'application/json'
+                    }
+                );
+                auth.login(responseData.userId, responseData.token);
+            } catch (err) {}
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={authSubmitHandler}>
             <h2>Please login</h2>
             <div className="field">
-                <div className="control">
-                    <input className="input is-primary" type="text" placeholder="Name" />
-                </div>
-            </div>
-            <div className="field">
                 <p className="control has-icons-left has-icons-right">
-                    <input className="input is-primary" type="email" placeholder="Email" />
+                    <input onChange={inputHandler} className="input is-primary" type="email" placeholder="Email" />
                     <span className="icon is-left">
                       <ion-icon name="mail-outline">Email</ion-icon>
                     </span>
                 </p>
             </div>
+            <div className="field">
+                <p className="control has-icons-left">
+                    <input onChange={inputHandler} className="input is-primary" type="password" placeholder="Password" />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                </p>
+            </div>
             <div className="control">
-                <button className="button is-link">Submit</button>
+                <button type="submit" className="button is-link">Submit</button>
             </div>
         </form>
     )
